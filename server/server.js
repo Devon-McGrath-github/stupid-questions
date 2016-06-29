3a8d59af2e50e100bd5a64bf9e060bb068ccdb62
sjennings.inc@gmail.com <eda@mac-mini-1.local>
2016-06-30 10:49:16 +1200

    Resolved pull requests questions

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ var PORT = process.env.PORT || 3000
 var app = express()
 
 app.use(bodyParser.json())
-app.use(cors({origin: 'http://localhost:8080'}))
 app.use(express.static(path.join(__dirname, 'static')))
 app.use('/things', things)
 
