d21ad947d5fe8a93d3976f72b295a890906e55a8
Abhi <abhi.kala123@gmail.com>
2016-06-30 14:56:37 +1200

    created questions component and have it rendered

--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-import Questions from './Questions'
-
 export default React.createClass({
   render () {
     return (
       <div>
-        <h1>Things</h1>
-        <Questions/>
+        <h1>Questions</h1>
       </div>
     )
   }
