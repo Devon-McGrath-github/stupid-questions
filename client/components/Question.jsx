9e51d903048d54f3456c98cd1f30d2bbc6380d41
Don Smith <locksmithdon@gmail.com>
2016-07-01 11:36:05 +1200

    Fixed lint errors

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import Answers from './Answers'
-import AddAnswer from './Add-Answer'
+import AddAnswer from './AddAnswer'
 
 export default React.createClass({
+  propTypes: {
+    question: React.PropTypes.array
+  },
   render () {
-
     return (
       <div>
-
         <div>
           <h1>Q:</h1>
           <p>{this.props.question.content}</p>
         </div>
-
         <Answers />
         <AddAnswer />
-
       </div>
     )
   }
