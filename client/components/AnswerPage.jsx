6310240f04a0ea75d67256311ae560d239c82076
Ben Southpaw <ben.dryden@gmail.com>
2016-06-30 15:42:16 +1200

    Answers to current question displaying as a list of answers

--- client/components/AnswerPage.jsx
+++ client/components/AnswerPage.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import questions from '../dummy/questions'
+import questionsData from '../dummy/questions'
+import answersData from '../dummy/answers'
 
 export default React.createClass({
   render () {
-    // placeholder for this.props that we will be given
-    let placeholder = questions[0].content
-    // replace with this.props *something*
-    //-------------
-
-    let currentQuestion = placeholder
+    let currentQuestion = questionsData[1]
+    let answers = 'placeholder'
 
     return (
       <div>
           <h1>Q:</h1>
-          <p>{currentQuestion}</p>
+          <p>{currentQuestion.content}</p>
 
-          
+          {/* probably going to want to put into seperate components */}
+          <h1>Answers:</h1>
+
+          <ul>
+              {answersData.map((answer) => {
+                  if (answer.questionId === currentQuestion.id) {
+                      return (
+                          <li key={answer.id}>
+                              {answer.content} {' '}
+                          </li>
+                      )
+                  }
+
+              })
+              }
+          </ul>
+
 
       </div>
     )
